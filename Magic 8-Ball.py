a1c436d53b41b19902f91e1e898cd32750ee79ea
KevinJohnson10 <144349448+KevinJohnson10@users.noreply.github.com>
2023-10-31 09:56:22 -0400

    Update Magic 8-Ball.py

--- Magic 8-Ball.py
+++ Magic 8-Ball.py
@@ -11,7 +11,12 @@ print(random_number)
 
 if random_number == 1:
   answer = "Yes - definitely"
-elif 
+elif random_number == 2:
+  answer = "It is decidedly so"
+elif random_number == 3:
+  answer = "Without a doubt"
+elif random_number == 4:
+  answer = "Reply hazy, try again"
 elif random_number == 5:
   answer = "Ask again later"
 elif random_number == 6:
